c4e7fca3036207bf6052cd4bebeee41f636b5371
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2024-04-09 11:42:52 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ echo "WASMEDGE installed"
 echo "Installing ContainerD and CRUN"
 curl -sSf https://raw.githubusercontent.com/sonichigo/wasmedge-demo-example/main/container.sh && source containerd.sh | bash
 echo "ContainerD + CRUN Installed"
+cd ../
 echo "Fetching for k8s"
-wget -qO- https://raw.githubusercontent.com/sonichigo/wasmedge-demo-example/main/install.sh && source install.sh | bash
+wget -qO- https://raw.githubusercontent.com/sonichigo/wasmedge-demo-example/main/install.sh && source k8s.sh | bash
